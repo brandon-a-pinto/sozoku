660723d21a97750f2069cadc8d85c606fc5401e2
Brandon Pinto <brandon.a.pinto@protonmail.com>
2023-07-31 15:47:40 -0300

    feat: add server connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var connection net.Conn
+
+	ip := flag.String("i", "127.0.0.1", "IP")
+	port := flag.String("p", "4444", "Port")
+	flag.Parse()
+
+	address := *ip + ":" + *port
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Server running (%s:%s)\n", *ip, *port)
+
+	connection, err = listener.Accept()
+	if err != nil {
+		log.Printf("[-] Connection refused\n")
+	} else {
+		log.Printf("[+] Connection established (%s)\n", connection.RemoteAddr().String())
+	}
 }
